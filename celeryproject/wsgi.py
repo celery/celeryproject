34869a19ff8e90b0fdc9f40bb2c9f5561e55815e
fireantology <fireantology@gmail.com>
2012-10-12 18:01:32 +0200

    Edit wsgi for match production server configuration

--- celeryproject/wsgi.py
+++ celeryproject/wsgi.py
@@ -13,7 +13,9 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
+
+sys.path.append('/siti/www.celeryproject.org/celeryproject')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
