fc8844f1cfc40eb3362e4b4818a78834187267ec
Mauro Rocco <mauro.rocco@kneip.com>
2013-10-14 11:21:49 +0200

    Move debug settings to local settings file, change favicon path

--- celeryproject/settings/base.py
+++ celeryproject/settings/base.py
@@ -9,9 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
