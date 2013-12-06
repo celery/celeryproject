be1cd4c36663514f8e3d9a41ff25ae308475a413
Mauro Rocco <fireantology@gmail.com>
2013-12-06 10:17:21 +0100

    Upgrade url for work with Django 1.6

--- celeryproject/urls.py
+++ celeryproject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 admin.autodiscover()
 
