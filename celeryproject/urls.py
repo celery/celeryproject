02a5be007697ec188be0e6360e39860da1c6c818
Asif Saif Uddin <auvipy@gmail.com>
2019-04-27 11:43:55 +0600

    urls formating

--- celeryproject/urls.py
+++ celeryproject/urls.py
@@ -4,28 +4,36 @@ from django.conf.urls.static import static
 from django.conf.urls import include, url
 from celeryweb import views
 
-urlpatterns = [url(r'^news/$', views.news, name='news'),
-               url(r'^news/(?P<slug>[\w-]*)', views.news, name='news_entry'),
-               url(r'^tutorials/$', views.tutorials, name='tutorials'),
-               url(r'^tutorials/(?P<slug>[\w-]*)',
-                   views.tutorials, name='tutorial'),
-               url(r'^community/$', views.community,
-                   name='external_resources'),
-               url(r'^profile/(?P<username>.+)/',
-                   views.user_profile, name='user_profile'),
+urlpatterns = [
+    url(r'^news/$', views.news, name='news'),
+    url(r'^news/(?P<slug>[\w-]*)', views.news, name='news_entry'),
+    url(r'^tutorials/$', views.tutorials, name='tutorials'),
+    url(
+        r'^tutorials/(?P<slug>[\w-]*)',
+        views.tutorials, name='tutorial'
+    ),
+    url(
+        r'^community/$', views.community,
+        name='external_resources'
+    ),
+    url(
+        r'^profile/(?P<username>.+)/',
+        views.user_profile, name='user_profile'),
 
-               # AJAX
-               url(r'^ajax/stackoverflowquestions/(?P<page_size>[0-9]+)/(?P<page>[0-9]+)/$',
-                   'stackoverflow_questions',
-                   name='stackoverflow_questions'),
-               url(r'^ajax/packageinfo/$',
-                   views.package_info,
-                   name='package_info'),
+    # AJAX
+    url(
+        r'^ajax/stackoverflowquestions/(?P<page_size>[0-9]+)/(?P<page>[0-9]+)/$',
+        views.stackoverflow_questions',
+        name='stackoverflow_questions'
+    ),
+    url(r'^ajax/packageinfo/$',
+        views.package_info,
+        name='package_info'),
 
-               # ADMIN
-               url(r'^admin/', include(admin.site.urls)),
-               url(r'^$', views.home, name='home'),
-               ] + static(
+    # ADMIN
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home, name='home'),
+] + static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
 
